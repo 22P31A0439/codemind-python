a6714028195a925c1f551e80a0b75ffc78230333
22P31A0439 <120160275+22P31A0439@users.noreply.github.com>
2023-11-03 15:59:37 +0530

    r,c = map(int,input().split()) mat = [] s = 0 f = 0 for i in range(r):     a = list(map(int,input().split()))     if i%2==0:         s = s+sum(a)     else:         f = f+sum(a) print(s,f,end = " ")

---
+++ Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.py
@@ -0,0 +1,11 @@
+r,c = map(int,input().split())
+mat = []
+s = 0
+f = 0
+for i in range(r):
+    a = list(map(int,input().split()))
+    if i%2==0:
+        s = s+sum(a)
+    else:
+        f = f+sum(a)
+print(s,f,end = " ")
