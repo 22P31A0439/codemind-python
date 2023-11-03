88434b15bf2cf8e643f765ab88ac344fa1f4d0e8
22P31A0439 <120160275+22P31A0439@users.noreply.github.com>
2023-11-03 15:35:47 +0530

    r,c = map(int,input().split()) mat = [] s = 0 f = 0 for k in range(r):     a = list(map(int,input().split()))     mat.append(a) for i in mat:     for j in i:         if j%2==0:             s = s+j         else:             f = f+j print(s,f,end ="")

---
+++ Sum_of_even_and__odd_elements_in_a_matrix.py
@@ -0,0 +1,14 @@
+r,c = map(int,input().split())
+mat = []
+s = 0
+f = 0
+for k in range(r):
+    a = list(map(int,input().split()))
+    mat.append(a)
+for i in mat:
+    for j in i:
+        if j%2==0:
+            s = s+j
+        else:
+            f = f+j
+print(s,f,end ="")
